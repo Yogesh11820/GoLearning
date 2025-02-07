ba70fd296482567b43f4db86106d19d5e77583bd
Yogesh Patil <yogeshpatil@Yogeshs-MacBook-Pro.local>
2025-02-07 13:08:17 +0530

    Add HelloWord and Variable types code

---
+++ VarTypes.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt")
+
+func main(){
+	var student1 string = "John" // datatype string
+	var student2 = "Jane" // datatype inferred
+	x := 2 // datatype inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+
+}
